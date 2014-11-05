fbf297bec83a4b4e49119f96f8e1d154d239a827
Kevin Zhang <zhangyongjiang@gmail.com>
2014-11-05 09:31:24 -0800

    Initial Commit

---
+++ iLayout/iLayout.h
@@ -0,0 +1,19 @@
+//
+//  iLayout.h
+//  iLayout
+//
+//  Created by Kevin Zhang on 11/5/14.
+//  Copyright (c) 2014 Gaoshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iLayout.
+FOUNDATION_EXPORT double iLayoutVersionNumber;
+
+//! Project version string for iLayout.
+FOUNDATION_EXPORT const unsigned char iLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iLayout/PublicHeader.h>
+
+
