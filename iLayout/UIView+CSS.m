1637bbc4adbbd2661ba980c1c351b76d945a64c4
Kevin Zhang <zhangyongjiang@gmail.com>
2014-11-10 20:21:31 -0800

    background img needs width and height

--- iLayout/UIView+CSS.m
+++ iLayout/UIView+CSS.m
@@ -675,6 +675,7 @@ static NSMutableDictionary* classCssCache;
 
 -(void)applyCssProperties {
     if(self.useCssLayout) {
+        [self applyCssSize];
         UIColor* bgColor = [self cssBgColor];
         if (bgColor) {
             self.backgroundColor = bgColor;
